528a291dab8a3c5b9d3520788de593ea02c648fa
LeoSame <leo.sam@ukr.net>
2021-01-16 23:25:35 +0200

    add training react

---
+++ react_Minin/minin-app/src/Loader.js
@@ -0,0 +1,5 @@
+import Ract from 'react'
+
+export default () => (
+  <div style={{display: 'flex', justifyContent: 'center', margin: '.5rem'}}><div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div>
+)

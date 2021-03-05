6efd516088d80f8a4f17bbecb2d38feb7cabb463
LeoSame <leo.sam@ukr.net>
2021-03-05 11:37:38 +0200

    add

--- redux_Minin/src/index.js
+++ redux_Minin/src/index.js
@@ -1,55 +1,57 @@
-import { applyMiddleware, createStore } from 'redux'
-import thunk from 'redux-thunk'
-import { rootReducer } from './redux/rootReducer'
-import { increment, decrement, asyncIncrement } from './redux/actions'
-import './styles.css'
+import { applyMiddleware, createStore, compose } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { rootReducer } from "./redux/rootReducer";
+import { increment, decrement, asyncIncrement, cahngeTheme } from "./redux/actions";
+import "./styles.css";
 
-const counter = document.getElementById('counter')
-const addBtn = document.getElementById('add')
-const subBtn = document.getElementById('sub')
-const asyncBtn = document.getElementById('async')
-const themeBtn = document.getElementById('theme')
+const counter = document.getElementById("counter");
+const addBtn = document.getElementById("add");
+const subBtn = document.getElementById("sub");
+const asyncBtn = document.getElementById("async");
+const themeBtn = document.getElementById("theme");
 
+// function logger(state) {
+//   return function (next) {
+//     return function (action) {
+//       console.log("Prev State", state.getState());
+//       console.log("Action", action);
+//       const newState = next(action);
+//       console.log("New State", newState);
+//       return newState;
+//     };
+//   };
+// }
 
-function logger(state) {
-  return function (next) {
-    return function(action) {
-      console.log('Prev State', state.getState())
-      console.log('Action', action)
-      const newState = next(action)
-      console.log('New State', newState)
-      return newState
-    }
-  }
-}
+const store = createStore(rootReducer, compose(applyMiddleware(thunk, logger)));
 
-const store = createStore(
-  rootReducer,
-  42,
-  applyMiddleware(thunk, logger)
-)
+addBtn.addEventListener("click", () => {
+  store.dispatch(increment());
+});
 
-addBtn.addEventListener('click', () => {
-  store.dispatch(increment())
-})
+subBtn.addEventListener("click", () => {
+  store.dispatch(decrement());
+});
 
-subBtn.addEventListener('click', () => {
-  store.dispatch(decrement())
-})
+asyncBtn.addEventListener("click", () => {
+  store.dispatch(asyncIncrement());
+});
 
-asyncBtn.addEventListener('click', () => {
-  store.dispatch(asyncIncrement())
-})
+themeBtn.addEventListener("click", () => {
+  const newTheme = document.body.classList.contains("light") ? "dark" : "light";
+  store.dispatch(cahngeTheme(newTheme));
+});
 
 store.subscribe(() => {
-  const state = store.getState()
+  const state = store.getState();
+  console.log(state);
 
-  counter.textContent = state
-})
+  counter.textContent = state.counter;
+  document.body.className = state.theme.value;
 
-store.dispatch({type: 'INIT_APPLICATION'})
+  [addBtn, subBtn, themeBtn, asyncBtn].forEach((btn) => {
+    btn.disabled = state.theme.disabled;
+  });
+});
 
-themeBtn.addEventListener('click', () => {
-  // document.body.classList.toggle('dark')
-})
-
+store.dispatch({ type: "INIT_APPLICATION" });
--- redux_Minin/src/redux/actions.js
+++ redux_Minin/src/redux/actions.js
@@ -1,21 +1,42 @@
-import { DECREMENT, INCREMENT, ASYNC_INCREMENT } from "./types";
+import { CHANGE_THEME, DECREMENT, DISABLE_BUTTONS, ENABLE_BUTTONS, INCREMENT } from "./types";
 
 export function increment() {
   return {
-    type: INCREMENT
-  }
+    type: INCREMENT,
+  };
 }
 
 export function decrement() {
   return {
-    type: DECREMENT
-  }
+    type: DECREMENT,
+  };
+}
+
+export function enableButtons() {
+  return {
+    type: ENABLE_BUTTONS,
+  };
+}
+
+export function disableButtons() {
+  return {
+    type: DISABLE_BUTTONS,
+  };
+}
+
+export function cahngeTheme(newTheme) {
+  return {
+    type: CHANGE_THEME,
+    payload: newTheme,
+  };
 }
 
 export function asyncIncrement() {
   return function (dispatch) {
+    dispatch(disableButtons());
     setTimeout(() => {
-      dispatch(increment())    
+      dispatch(increment());
+      dispatch(enableButtons());
     }, 1500);
-  }
+  };
 }

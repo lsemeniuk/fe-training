83949beb574650c000cf8a51fa21dc8c11f4e15c
LeoSam <leo.sam@ukr.net>
2021-04-06 01:42:43 +0300

    work to online store

--- online-store-course/server/db.js
+++ online-store-course/server/db.js
@@ -1,12 +1,12 @@
-const {Sequelize} = require('sequelize')
+const { Sequelize } = require("sequelize");
 
 module.exports = new Sequelize(
-    process.env.DB_NAME, // Название БД
-    process.env.USER, // Пользователь
-    process.env.DB_PASSWORD, // Пароль
-    {
-        dialect: 'postgres',
-        host: process.env.DV_HOST,
-        port: process.env.DB_PORT
-    }
-)
+  process.env.DB_NAME, // Название БД
+  process.env.DB_USER, // Пользователь
+  process.env.DB_PASSWORD, // Пароль
+  {
+    dialect: "postgres",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+  }
+);
--- online-store-course/server/index.js
+++ online-store-course/server/index.js
@@ -1,8 +1,23 @@
-require('dotenv').config()
-const express = require('express')
+require("dotenv").config();
+const express = require("express");
+const sequelize = require("./db");
+const models = require("./models/models");
+const cors = require("cors");
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();

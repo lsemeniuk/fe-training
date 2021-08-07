a889374e54ed663ff23a6c455132b09a33f370f6
Leonid Semeniuk <leo.sam@ukr.net>
2021-08-08 00:44:35 +0300

    add page repos, search  repos, axios request

--- Ulbi_project/ReactReduxApp/src/components/App.jsx
+++ Ulbi_project/ReactReduxApp/src/components/App.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setCount } from '../reducers/reposReducer';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Main from './main/Main';
 import './App.less';
 
 const App = () => {
-  const dispatch = useDispatch();
-  const count = useSelector(state => state.repos.count);
-
-  function onCountClick() {
-    dispatch(setCount(5));
-  }
-
   return (
-    <div className='app'>
-      <button onClick={() => onCountClick()}>COUNT</button>
-      <div>{count}</div>
-    </div>
+    <BrowserRouter>
+      <div className='container'>
+        <Route path='/' component={Main} />
+      </div>
+    </BrowserRouter>
   );
 };
 

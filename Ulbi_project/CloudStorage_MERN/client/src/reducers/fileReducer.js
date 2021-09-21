eb630c538418643eb43d7383b15e4411d2fcab31
Leonid Semeniuk <leo.sam@ukr.net>
2021-09-21 21:46:14 +0300

    add store

---
+++ Ulbi_project/CloudStorage_MERN/client/src/reducers/fileReducer.js
@@ -0,0 +1,11 @@
+const defaultsState = {
+
+}
+
+export default function fileReducer(state = defaultsState, action) {
+  switch (action.type) {
+
+    default:
+      return state
+  }
+}
---
+++ Ulbi_project/CloudStorage_MERN/client/src/reducers/userReducer.js
@@ -0,0 +1,12 @@
+const defaultsState = {
+  currentUser: {name: 'Leonid'},
+  isAuth: false
+}
+
+export default function userReducer(state = defaultsState, action) {
+  switch (action.type) {
+
+    default:
+      return state
+  }
+}

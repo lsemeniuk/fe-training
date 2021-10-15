9a661cfc40f0727d36824ee7635bab0152cac284
Leonid Semeniuk <l.semeniuk@astoundcommerce.com>
2021-10-15 15:26:18 +0300

    add space

--- Ulbi_project/CloudStorage_MERN/client/src/actions/user.js
+++ Ulbi_project/CloudStorage_MERN/client/src/actions/user.js
@@ -34,6 +34,7 @@ export const auth = () => {
       const response = await axios.get(`http://localhost:5000/api/auth/auth`, {
         headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
       });
+
       dispatch(setUser(response.data.user));
       localStorage.setItem('token', response.data.token);
     } catch (e) {

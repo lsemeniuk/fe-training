f9c8cf4529a34f0afec8b02a104eab6e1f1718a2
Leonid Semeniuk <leo.sam@ukr.net>
2022-01-31 23:10:27 +0200

    Learn component Transition

--- Ulbi_project/react-animations/src/App.jsx
+++ Ulbi_project/react-animations/src/App.jsx
@@ -1,13 +1,38 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Transition } from 'react-transition-group';
 import './App.css';
 
 function App() {
   const [loaderVisible, setLoaderVisible] = useState(false);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaderVisible(true);
+    }, 1000);
+    setTimeout(() => {
+      setLoaderVisible(false);
+    }, 5000);
+  }, []);
+
   return (
     <div className='App'>
       <button onClick={() => setLoaderVisible(!loaderVisible)}>{loaderVisible ? 'hide' : 'show'}</button>
-      <div className='wrap'>{loaderVisible && <div className='circle' />}</div>
+      <div className='wrap'>
+        <Transition
+          in={loaderVisible}
+          timeout={500}
+          mountOnEnter
+          unmountOnExit
+          onEnter={() => console.log('onEnter')}
+          onEntering={() => console.log('onEntering')}
+          onEntered={() => console.log('onEntered')}
+          onExit={() => console.log('onExit')}
+          onExiting={() => console.log('onExiting')}
+          onExited={() => console.log('onExited')}
+        >
+          {state => <div className={`circle ${state}`} />}
+        </Transition>
+      </div>
     </div>
   );
 }

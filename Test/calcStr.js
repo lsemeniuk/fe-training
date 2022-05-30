19856404c3dcb23b4e69f75cfca13302a3a1f59a
Leonid Semeniuk <leo.sam@ukr.net>
2022-05-30 13:03:05 +0300

    add funk calc page to print books

---
+++ Test/calcStr.js
@@ -0,0 +1,32 @@
+const pages = 300;
+const letterNotebookPages = 10;
+const pagesNotebook = letterNotebookPages * 4;
+
+const finalArr = [];
+
+let notebooks = Math.ceil(pages / pagesNotebook);
+
+for (let i = 1; i <= notebooks; i++) {
+  let firstPage = (i - 1) * pagesNotebook + 1;
+  let lastPage;
+  i === notebooks ? (lastPage = pages) : (lastPage = i * pagesNotebook);
+
+  const strFront = [];
+  const strBack = [];
+
+  if (i !== notebooks) {
+    for (let p = 0; p < letterNotebookPages; p++) {
+      strFront.push(lastPage - p * 2, firstPage + p * 2);
+      strBack.push(firstPage + 1 + p * 2, lastPage - 1 - p * 2);
+    }
+  } else {
+    let lastLatterNotebook = (lastPage - firstPage + 1) / 4;
+
+    for (let p = 0; p < lastLatterNotebook; p++) {
+      strFront.push(lastPage - p * 2, firstPage + p * 2);
+      strBack.push(firstPage + 1 + p * 2, lastPage - 1 - p * 2);
+    }
+  }
+  finalArr.push({ strFront, strBack });
+}
+console.log(finalArr, 'notebooks', notebooks);
